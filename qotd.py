332f00b9aed58f13b51c2267a5371209e8314bf5
Matthew Farrugia-Roberts <matt@far.in.net>
2020-04-30 20:00:37 +0200

    qotd server implementation

---
+++ qotd.py
@@ -0,0 +1,62 @@
+import csv
+import os
+import random
+import socket
+import time
+
+
+HOST = ""
+PORT = 17
+FILE = "quotes.txt"
+
+def main():
+    # load quote list
+    print("loading quotes from", FILE)
+    q = QuoteList(FILE)
+
+    # set up server socket
+    print("setting up server socket, port", PORT)
+    ssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    ssock.bind((HOST, PORT))
+    ssock.listen()
+    print("listening... (^C to stop)")
+    # process incoming connections
+    while True:
+        sock, addr = ssock.accept()
+        print(addr, "connected...", end="", flush=True)
+        quote = q.qotd()
+        sock.sendall(quote.encode())
+        sock.close()
+        print(" quote sent!")
+
+
+class QuoteList:
+    def __init__(self, quotes_file):
+        self.filename = quotes_file
+        self._load()
+    def _load(self):
+        self.quotes = []
+        self.loadtime = time.time()
+        with open(self.filename) as f:
+            text = f.read()
+        pars = text.split('\n\n')
+        for p in pars:
+            lines = [l for l in p.splitlines() if l and not l[0]=="#"]
+            if not lines: continue
+            quote = ' '.join(lines)
+            self.quotes.append(quote)
+        print("loaded", len(self.quotes), "quotes")
+    def qotd(self):
+        # check if reload needed
+        if self.loadtime < os.path.getmtime(self.filename):
+            self._load()
+        return random.choice(self.quotes)
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nbye!")
